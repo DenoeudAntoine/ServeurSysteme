5410d836a438946872fe5861836c3ad88d2e6c2b
Denoeud Antoine <denoeudantoine@laposte.net>
2016-02-10 15:56:18 +0100

    fork1

--- webserver/Main.c
+++ webserver/Main.c
@@ -7,22 +7,27 @@
 int main(void)
 {
   int socket_serveur;
+    initialiser_signaux();
    socket_serveur = creer_serveur(8080);
+  
     if(socket_serveur == -1) {
       perror("créer_serveur");
       exit(1);
     }
 
     while(1) {
-      int socket_client ;
-      
+      int socket_client;
+      int pid;
       socket_client = accept(socket_serveur,NULL,NULL);
+     
       if(socket_client == -1)
 	{
 	  perror ("accept");
 	  /* traitement d ’ erreur */
+	  return 1;
 	}
-      initialiser_signaux();
+      pid = fork();
+      if(pid == 0) {
       /* On peut maintenant dialoguer avec le client */
       const char * message_bienvenue = "Bonjour,bienvenue sur mon serveur\n c'est cool \n toto \n plus d'idées \n bon \n c'est \n bientot \n fini \n ! \n ou pas \n ah si.\n";
       sleep(1);
@@ -33,8 +38,9 @@ int main(void)
 	write(socket_client,&message_client,50);
         memset(message_client, 0, sizeof(message_client));
       }
-
-     
+      } else {
+	close(socket_client);
+      }
       
       
     }
