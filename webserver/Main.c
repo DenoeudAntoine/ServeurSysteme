038dd9c900b8f9b3db02685d7e1388421d3ab44b
Antoine Denoeud <denoeudantoine@laposte.net>
2016-04-01 17:56:32 +0200

    simple_get

--- webserver/Main.c
+++ webserver/Main.c
@@ -7,104 +7,126 @@
 
 int main(void)
 {
+  
+  const char* msgErreur400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
+  const char* msg200 = "HTTP/1.1 200 OK\r\nContent-Length: ";
+  const char * message_bienvenue = "Bonjour,bienvenue sur mon serveur Pawnee !\n";
+  
+  char buff[1024];
   int socket_serveur;
-    initialiser_signaux();
-   socket_serveur = creer_serveur(9876);
   
-    if(socket_serveur == -1) {
-      perror("créer_serveur");
-      exit(1);
-    }
-     int socket_client;
-    while(1) {
-     
-      int pid;
-      socket_client = accept(socket_serveur,NULL,NULL);
-     
-      if(socket_client == -1)
-	{
-	  perror ("accept");
-	  /* traitement d ’ erreur */
-	  return 1;
-	}
-      pid = fork();
-      if(pid == 0) {
-      /* On peut maintenant dialoguer avec le client */
-     
-      /* char message_client[50];
-      memset(message_client, 0, sizeof(message_client));
-      while(read(socket_client,&message_client,50)>0){
-	write(socket_client,&message_client,50);
-        memset(message_client, 0, sizeof(message_client));
-	}*/
-
-      FILE *file = fdopen(socket_client,"w+");
-      char buff[1024];
-      fgets(buff,1024,file);
-       
-      char *mots;
-      char tmp[1024];
-      strcpy(tmp,buff);
-      mots = strtok(tmp," ");
-
-   
-      if(strcmp(mots,"GET") != 0)
-	return 0;
-
-      mots = strtok(NULL," ");
-      mots = strtok(NULL," ");
-      if(strcmp(mots,"HTTP/1.0") == 0 || strcmp(mots,"HTTP/1.1") == 0)
-	return 0;
-        printf("<Pawnee> %s",buff);
-      memset(buff,0,sizeof(buff));
-     
-
-       const char * message_bienvenue = "Bonjour,bienvenue sur mon serveur Pawnee !\n";
-      sleep(1);
-      write(socket_client,message_bienvenue,strlen(message_bienvenue)+1);
-
-      
-      while(1){
-	fgets(buff,1024,file);
-	printf("<Pawnee> %s",buff);
-	memset(buff,0,sizeof(buff));
-      }
-      
-      } else {
-	close(socket_client);
-      }
-      
-      
-    }
-
-    close(socket_serveur);
-    
-    return 0;
-}
-
-void messageErreur(void){
   
-}
+  
+ 
+  socket_serveur = creer_serveur(9876);
 
-void traitement_signal(int sig){
-  printf("Signal %d reçu\n", sig);
-  int status = 0;
-  while(waitpid(-1,&status,WNOHANG)!=-1){
+ initialiser_signaux();
+  
+  if(socket_serveur == -1) {
+    perror("créer_serveur");
+    exit(1);
   }
+  int socket_client;
+  while(1) {
+    
+    int pid;
+    socket_client = accept(socket_serveur,NULL,NULL);
+    
+    if(socket_client == -1)
+      {
+	perror ("accept");
+	/* traitement d ’ erreur */
+	return 1;
+      }
+     FILE *file = fdopen(socket_client,"w+");
+    pid = fork();
+    if(pid == 0) {
+      /* On peut maintenant dialoguer avec le client */
+      
+      /* char message_client[50];
+	 memset(message_client, 0, sizeof(message_client));
+	 while(read(socket_client,&message_client,50)>0){
+	 write(socket_client,&message_client,50);
+	 memset(message_client, 0, sizeof(message_client));
+	 }*/
+      
+      fgets(buff,1024,file);
+      if(verifierEntete(buff) == 1){
+	
+	fprintf(file, msg200, strlen(msg200));
+	fprintf(file, "%zu\r\n\r\n", strlen(message_bienvenue));
+        fprintf(file, message_bienvenue, strlen(message_bienvenue));
+	
+	while(1){
+	  /*fgets(buff,1024,file);
+		printf("<Pawnee> %s",buff);
+		memset(buff,0,sizeof(buff));*/
+	}
+	
+      } else {
+	fprintf(file, msgErreur400, strlen(msgErreur400));
+	close(socket_client);
+	return 1;
+      }
+    } else {
+      close(socket_client);
+      wait(NULL);
+    }
+    
+    
+  }
+  close(socket_serveur);
+  
+  return 0;
 }
 
+  int verifierEntete(char * str)
+  {
+
+  char *mots;
+  char *tmp;
+  tmp = strdup(str);
+  mots = strtok(tmp," ");
+  
+  
+  if(strcmp(mots,"GET") != 0)
+    return 0;
+    
+
+  mots = strtok(NULL," ");
+  mots = strtok(NULL," ");
+  if(strcmp(mots,"HTTP/1.0") == 0 || strcmp(mots,"HTTP/1.1") == 0)
+    return 0;
+  
+  printf("%s\n",str);
+  return 1;
+}
+
+void traitement_signal(int sig)
+{
+  printf("Signal %d reçu\n",sig);
+  /* attend un fils */
+  if(sig == SIGCHLD)
+    {
+      if(waitpid(-1, NULL, 0) == -1)
+	perror("waitpid");
+    }
+  /* ignore les SIGPIPE */
+  else if(sig == SIGPIPE)
+    if(signal(sig, SIG_IGN) == SIG_ERR)
+      perror("signal(SIGPIPE, SIG_IGN)");
+}
 void initialiser_signaux(void)
 {
-
-  if(signal(SIGPIPE,SIG_IGN) == SIG_ERR)
-    perror("initialiser signaux");
-
   struct sigaction sa;
-
+  /* handler */
   sa.sa_handler = traitement_signal;
+  /* ensemble des signaux vides */
   sigemptyset(&sa.sa_mask);
+  /* option par default */
   sa.sa_flags = SA_RESTART;
-  if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+  if(sigaction(SIGPIPE, &sa, NULL) == -1)
+    perror("sigaction(SIGPIPE)");
+  if(sigaction(SIGCHLD, &sa, NULL) == -1)
     perror("sigaction(SIGCHLD)");
-  }
 }
